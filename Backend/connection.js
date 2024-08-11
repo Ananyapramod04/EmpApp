9b4663963044070067350b66bb179a6700300f23
Ananya <ananyapramod04@gmail.com>
2024-08-11 11:58:34 +0530

    first commit

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,6 +1,6 @@
 
   const mongoose=require('mongoose');
-  mongoose.connect('mongodb+srv://aromalm2310:9605467664@cluster0.3lutxiz.mongodb.net/employeedb?retryWrites=true&w=majority&appName=Cluster0 ').then((res)=>{
+  mongoose.connect('mongodb+srv://ananyapramod04:8136988796@cluster0.nu5shqq.mongodb.net/employeedb?retryWrites=true&w=majority&appName=Cluster0 ').then((res)=>{
       console.log('DB is connected');
   }).catch((error)=>{
       console.log('Error in connection')
